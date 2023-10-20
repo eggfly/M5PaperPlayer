050073ff7c85e8b6a1b824164d7e1105c48509cf
eggfly <lihaohua90@gmail.com>
2023-10-20 23:09:05 +0800

    Add pins

--- I2Saudio/config.h
+++ I2Saudio/config.h
@@ -5,6 +5,12 @@
 #define SPI_MISO      13
 #define SPI_SCK       14
 
-#define I2S_DOUT      18
-#define I2S_BCLK      19
-#define I2S_LRC       32
+//Internal Mod Pins
+//#define I2S_DOUT      18
+//#define I2S_BCLK      19
+//#define I2S_LRC       32
+
+// External 2-Grove Pins
+#define I2S_DOUT      19
+#define I2S_BCLK      33
+#define I2S_LRC       18
